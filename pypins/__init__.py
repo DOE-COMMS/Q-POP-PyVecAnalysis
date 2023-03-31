043daadc98c08f1dbfe71e2a9b9107189b79b5a8
cxx <0001001bill@gmail.com>
2023-03-31 17:56:10 -0500

    initial commit based on Jacob Zorn's version
    https://gitlab.com/jazorn/pyvecanalysis

---
+++ pypins/__init__.py
@@ -0,0 +1,1 @@
+from .PyPINS import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name='PyPINS',
+	version='0.5.0',
+	author='Jacob Zorn',
+	author_email='jzz164@psu.edu',
+	description='Python Programmatic Identification of Novel Structures',
+	packages=setuptools.find_packages())
